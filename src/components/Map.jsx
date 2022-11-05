94802bd0e2a05f06dfb6516c9a041c4a3347ba03
jamal ahmad <jamalcs007@gmail.com>
2022-11-05 09:09:13 +0500

    changes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,17 @@
 import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map = () => {
   const { addressData, isLoading } = useContext(GlobalContext);
